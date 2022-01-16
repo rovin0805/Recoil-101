3039e3be72f196f83013eb3551f7722bba4baee1
Choi Eun Jeong <qhdrn67@naver.com>
2022-01-17 00:09:05 +0900

    setup hook form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,24 +1,34 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  toDo: string;
+}
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    setValue,
+  } = useForm<IForm>({
+    defaultValues: {},
+  });
 
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
+  const handleValid = (data: IForm) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", "");
   };
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register("toDo", {
+            required: "Please write a To Do",
+          })}
+          placeholder="Write a to do"
+        />
         <button>Add</button>
       </form>
     </div>
