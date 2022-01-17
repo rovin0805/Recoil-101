670c2d805ea27793cc006b7c06ffa894521a9c83
Choi Eun Jeong <qhdrn67@naver.com>
2022-01-17 21:38:52 +0900

    recoil selector part 1

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,12 +1,24 @@
-import { atom } from 'recoil';
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   id: number;
   text: string;
-  category: 'TO_DO' | 'DOING' | 'DONE';
+  category: "TO_DO" | "DOING" | "DONE";
 }
 
 export const toDoState = atom<IToDo[]>({
-  key: 'toDo',
+  key: "toDo",
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    return [
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE"),
+    ];
+  },
 });
